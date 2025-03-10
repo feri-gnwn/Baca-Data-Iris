66ef8b6f9e9b43187534ca2ff4155eadad442e9c
unknown <ferigunawan1505@gmail.com>
2025-03-10 23:06:04 +0800

    nambahin komentar penjelasan

--- bacaIris.cpp
+++ bacaIris.cpp
@@ -6,34 +6,34 @@ using namespace std;
 class BacaIris
 {
 private:
-    ifstream ifile;
-    string namafile;
+    ifstream ifile; // pointer to file stream
+    string namafile; // nama file yang dibaca
 
 public:
-    BacaIris(string namafile) { this->namafile = namafile; }
-    void bukaFile() { ifile.open(namafile); }
-    void fileRead();
-    ~BacaIris() { ifile.close(); }
+    BacaIris(string namafile) { this->namafile = namafile; } // constructor
+    void bukaFile() { ifile.open(namafile); } // Fungsi untul membuka file
+    void fileRead(); // Fungsi untuk membaca file
+    ~BacaIris() { ifile.close(); } // Destructor untuk menutup file
 };
 
-void BacaIris::fileRead()
+void BacaIris::fileRead() 
 {
-    string baris;
+    string baris; // variabel untuk menyimpan baris yang dibaca
 
-    if (ifile.fail())
+    if (ifile.fail()) // jika file gagal dibuka
     {
-        cerr << "Error: Gagal membuka file" << endl;
+        cerr << "Error: Gagal membuka file" << endl; //Tampilan pesan yang akan dikeluarkan jika file gagal dibuka.
     }
 
-    while(getline( ifile, baris, ','))
+    while(getline( ifile, baris, ',')) // membaca data-data dari file yang dibatasi dengan delimiter koma (,) akan disimpan di variabel baris
     {
-        cout << baris << endl;
+        cout << baris << endl; // tampilkan baris yang telah dibaca
     }
 }
 
-int main()
+int main()  
 {
-    BacaIris iris("iris.data");
+    BacaIris iris("iris.data"); // Membuat objek iris dengan nama file iris.data
 
     iris.bukaFile();
     iris.fileRead();
